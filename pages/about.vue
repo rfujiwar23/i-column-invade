d7c77643bdfad6bccb2fe4f0f6991c86d8543185
Ryo Fujiwara <jjcproject@icloud.com>
2021-09-09 22:43:28 +0900

    about page, items

--- pages/about.vue
+++ pages/about.vue
@@ -2,49 +2,77 @@
         
        <div>
            <Top />
-            <div class="container-fluid">
-            <div class="row">
+            <div class="container-fluid p-0">
+            <div class="top-main">
+                <div class="about">
+                    <h2>紹介している製品一覧</h2>
+                    <p>このコラムサイトでは<a href="https://ifing-beauty.com/">IFING BEAUTY ONLINE</a>及び<a href="#">fofo</a>で販売している製品について紹介しています。<br>
+                    クリックすると商品の説明ページへ飛びます。
+                    </p>
+                </div>
+            </div>
+            <div class="row mx-0">
                 <div class="p-0 col-xl-3 col-lg-3 col-md-6 col-sm-6 image">
+                   <a href="https://ifing-beauty.com/products/list?category_id=7">
                     <div class="overlay"></div>
+                    <div class="top-title">
+                        トキオ・インカラミ
+                    </div>
                     <div class="centered">
                         <!-- TOKIO INKARAMI
                          -->
                         <img src="@/assets/tokio-logo.svg" alt="TOKIO INKARAMI"> 
                     </div>
                     <img src="@/assets/003-tokio.jpg" alt="TOKIO INKARAMI" class="fit-in-div">
-                    
+                   </a>
                 </div>
                 <div class="p-0 col-xl-3 col-lg-3 col-md-6 col-sm-6 image">
+                    <a href="#">
                     <div class="overlay"></div>
+                    <div class="top-title">
+                        オーウェイ
+                    </div>
                     <div class="centered">
                         <img src="@/assets/oway-logo.png" alt="TOKIO INKARAMI">
                         <!-- OWAY -->
                     </div>
                     <img src="@/assets/001-oway.jpg" alt="TOKIO INKARAMI" class="fit-in-div">
+                    </a>
                 </div>
                 <div class="p-0 col-xl-3 col-lg-3 col-md-6 col-sm-6 image">
+                    <a href="#">
                     <div class="overlay"></div>
+                    <div class="top-title">
+                        ケビン・マーフィー
+                    </div>
                     <div class="centered">
                         <img src="@/assets/kevinmurphy-logo.svg" alt="TOKIO INKARAMI">
                     </div>
                     <img src="@/assets/004-kevinmurphy.jpg" alt="TOKIO INKARAMI" class="fit-in-div">
+                    </a>
                 </div>
                 <div class="p-0 col-xl-3 col-lg-3 col-md-6 col-sm-6 image">
+                    <a href="#">
                     <div class="overlay"></div>
+                    <div class="top-title">
+                        アジア
+                    </div>
                     <div class="centered">
                       <img src="@/assets/asia-logo.svg" alt="TOKIO INKARAMI"> 
                     </div>
                     <img src="@/assets/002-asia.jpg" alt="TOKIO INKARAMI" class="fit-in-div">
+                    </a>
                 </div>
                 <div class="p-0 col-xl-3 col-lg-3 col-md-6 col-sm-6 image">
-                    <div class="overlay"></div>
+                   <a href="#">
+                        <div class="overlay"></div>
                     <div class="centered">
                         <!-- TOKIO INKARAMI
                          -->
                         <img src="@/assets/tokio-logo.svg" alt="TOKIO INKARAMI"> 
                     </div>
                     <img src="@/assets/003-tokio.jpg" alt="TOKIO INKARAMI" class="fit-in-div">
-                    
+                   </a>
                 </div>
                 <div class="p-0 col-xl-3 col-lg-3 col-md-6 col-sm-6 image">
                     <div class="overlay"></div>
@@ -107,6 +135,32 @@
 
 <style lang="scss">
 
+.top-main {
+    background: rgb(174,158,146);
+    background: linear-gradient(320deg, rgba(174,158,146,1) 0%, rgba(252,237,225,1) 100%);
+    .about {
+        padding:4vh 0;
+        h2 {
+            margin-bottom:3vh;
+            margin-left:2vw;
+        }
+
+        p {
+            margin-left:3vw;
+            line-height:2em;
+            a {
+                padding:0 3px;
+                // text-decoration: underline;
+                color:rgba(30, 139, 195, 1);
+                &:hover {
+                    padding-bottom:2px;
+                    border-bottom:1px solid rgba(30, 139, 195, 1);
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+}
 
 .image {
     position:relative;
@@ -121,6 +175,19 @@
         height:100%;
         z-index: -1;
     }
+    .top-title {
+        position:absolute;
+        top:0;
+        right:0;
+        left:0;
+        z-index:1000;
+        background-color: rgba(20,20,20,0.4);
+        // background:rgba(255,255,255,0.6);
+        text-align: center;
+        // color:rgb(255,255,255);
+        color: white;
+        font-size:1.25em;
+    }
     .overlay {
         height:100%;
         background:rgba(20,20,20,0.2);
@@ -134,7 +201,7 @@
             background:rgba(20,20,20,0.5);
         }
     }
-    .centered {
+    .centered { 
         position:absolute;
         top:50%;
         bottom:50%;
@@ -146,6 +213,35 @@
         z-index:999;
         font-weight:bold;
         
+    }
+}
+
+@media screen and (max-width:768px) {
+    .top-main {
+        .about {
+            h2 {
+                font-size:1.5em;
+            }
+            p {
+                font-size:0.9em;
+                padding:0 20px;
+            }
+        }
+    }
+}
+
+@media screen and (max-width:500px) {
+    .top-main {
+        .about {
+            h2 {
+                font-size:1.25em;
+            }
+            p {
+                font-size:0.8em;
+                padding:0 30px;
+                margin:0;
+            }
+        }
     }
 }
 </style>
