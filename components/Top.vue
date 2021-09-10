bfb07808289c354e46526efb09e3ab30ca192e80
Ryo Fujiwara <jjcproject@icloud.com>
2021-09-10 22:39:44 +0900

    upload with about page routes

--- components/Top.vue
+++ components/Top.vue
@@ -9,15 +9,17 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto">
-          <!-- <li class="nav-item active">
-            <a class="nav-link" href="#">IFING BEAUTY <span class="sr-only">(current)</span></a>
-          </li> -->
+          <li class="nav-item">
+            <nuxt-link to="/about">About</nuxt-link>
+          </li>
           
           <li class="nav-item">
-            <a class="nav-link" href="http://ifing-beauty.com/" target="_blank">IFING BEAUTY ONLINE</a>
+            <!-- <a class="nav-link" href="http://ifing-beauty.com/" target="_blank">IFING BEAUTY ONLINE</a> -->
+            <nuxt-link to="http://ifing-beauty.com/">IFING BEAUTY</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" target="_blank">FOFO</a>
+            <!-- <a class="nav-link" href="#" target="_blank">FOFO</a> -->
+            <nuxt-link to="http://ifing-beauty.com/">FOFO</nuxt-link>
           </li>
           <!-- <li class="nav-item">
             <a class="nav-link" href="https://www.ifing.com" target="_blank">IFING</a>
@@ -48,6 +50,15 @@
 .img {
   width:120px;
   margin-left:-10px;
+}
+
+.navbar-nav {
+  .nav-item {
+    margin-right:20px;
+    a {
+      color:#9a9a9a;
+    }
+  }
 }
 
 @media screen and (max-width:992px) {
