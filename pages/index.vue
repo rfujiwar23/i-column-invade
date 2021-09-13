580519c857207bd05ce2ca2acb69f8b0ae3f780f
Ryo Fujiwara <jjcproject@icloud.com>
2021-09-13 20:11:05 +0900

    upload: no text preview

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@
                           </div>
                           <div class="col-9">
                             <h2>{{content.title}}</h2>
-                            <p class="mobile-body" v-html="content.body"></p>
+                            <!-- <p class="mobile-body" v-html="content.body"></p> -->
                             <p class="link-to"><span>#{{content.category.name}}</span></p>
                             <p class="link-to"><nuxt-link :to="`/${content.id}`">続きを読む →</nuxt-link></p>
                           </div>
@@ -52,7 +52,7 @@
                           <h5 class="card-title"><nuxt-link :to="`/${content.id}`">{{ content.title }}</nuxt-link></h5>
                           <small class="text-muted">{{ content.publishedAt.substring(0,10) }}</small>
                           <h6 class="card-category"><span>#{{content.category.name}}</span></h6> 
-                          <p class="card-text" v-html="content.body"></p>
+                          <!-- <p class="card-text" v-html="content.body"></p> -->
                           <!-- <p v-html="content.body" class="text-me"></p> -->
                         </div>
                         <div class="card-footer">
@@ -406,6 +406,8 @@ export default {
     }
     p.link-to {
       font-size:0.7em;
+      line-height:0.8em;
+      margin-top:10px;
       span {
         padding:3px 5px;
         background:#9a9a9a;
