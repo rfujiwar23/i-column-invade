60147db11c6f5164c0a9fc75cddb2dade28058c1
Ryo Fujiwara <jjcproject@icloud.com>
2021-10-07 18:30:28 +0900

    upload me

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -217,6 +217,7 @@ export default {
         p {
           line-height: 1.8;
           letter-spacing: 0.2px;
+          font-size:1.25em;
           img {
             object-fit: contain;
             max-width: 100%;
@@ -294,7 +295,7 @@ export default {
       }
       .post {
         > p {
-          font-size:0.9em;
+          font-size:1.1em;
         }
       }
     }
