fedd51b37572c3aefd20804e5e795b394e018c78
Ryo Fujiwara <jjcproject@icloud.com>
2022-05-12 18:39:39 +0900

    remove a word for SEO purpose

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -10,7 +10,7 @@
       <!-- <Banner /> -->
       <div class="title-area mb-0">  
             <SocialMedia />
-            <h1>IFING BEAUTY/fofoのコラムページ</h1>
+            <h1>IFING BEAUTYのコラムページ</h1>
       </div>
       
     </div>
